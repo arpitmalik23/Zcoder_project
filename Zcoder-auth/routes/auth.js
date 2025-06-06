fbf3fb7319a1fb52c83694588d7bb39f59637c5b
arpitmalik23 <m.arpit@iitg.ac.in>
2025-06-06 20:02:07 +0530

    added signup page

---
+++ Zcoder-auth/routes/auth.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
+const router = express.Router();
+
+
+// Signup route
+router.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(400).json({ message: "User already exists" });
+
+    const hashed = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hashed });
+    await newUser.save();
+
+    res.status(201).json({ message: "Signup successful" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// Login route
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(400).json({ message: "Invalid credentials" });
+
+    res.status(200).json({ message: "Login Successful" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+module.exports = router;
---
+++ Zcoder-auth/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+const authRoutes = require("./routes/auth");
+app.use("/api", authRoutes);
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("MongoDB Connected");
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+})
+.catch((err) => console.log(err));
